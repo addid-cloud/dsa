c6d78bfd12e78af7defe5bf0294185eacf19cfb4
evifrn <72894223+addid-cloud@users.noreply.github.com>
2025-09-02 05:07:34 +0000

    p1 - array

---
+++ HighArrayApp.java
@@ -0,0 +1,82 @@
+class HighArray{
+    private int[] arr;
+    private int nElemen;
+    public HighArray(int max){
+        arr = new int[max] ;
+        nElemen = 0;
+    }
+public void insert(int value){
+    int i ;
+    for (i = nElemen - 1; i >= 0; i--) {
+        arr[i + 1] = arr[i]; 
+    }
+    arr[i + 1] = value; 
+    // arr[nElemen] = value;
+    nElemen++;
+}
+    public boolean find(int key){
+        for (int i = 0; i < nElemen; i++) {
+            if(arr[i] == key){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return false;
+    }
+    public int size(){
+        return arr.length;
+    }
+    public boolean delete(int value){
+        for (int i = 0; i < nElemen; i++) {
+            if(value == arr[i]){
+                break;
+            }
+            if(i == nElemen){
+                return false;
+            }else{
+                for (int j = 0; j < nElemen; j++) {
+                    arr[j] = arr[j+1];
+                }
+                nElemen--;
+                return true;
+            }
+        }return false;
+    }
+        public void display() { 
+        for (int i = 0; i < nElemen; i++) { 
+            System.out.print(arr[i] + " "); 
+        } 
+        System.out.println(""); 
+    } 
+}
+
+public class HighArrayApp { 
+    public static void main(String[] args) { 
+    int maxSize = 100; 
+    HighArray arr; 
+    arr = new HighArray(maxSize); 
+    arr.insert(70); 
+    arr.insert(80); 
+    arr.insert(75); 
+    arr.insert(55); 
+    arr.insert(85); 
+    arr.insert(25); 
+    arr.insert(30); 
+    arr.insert(00); 
+    arr.insert(90); 
+    arr.insert(40) ;
+    arr.display(); 
+    int key = 25; 
+    if (arr.find(key)) { 
+        System.out.println(key + " ditemukan"); 
+    } else { 
+        System.out.println(key + " tidak ditemukan"); 
+    } 
+    arr.delete(00); 
+    arr.delete(80); 
+    arr.delete(55); 
+    arr.insert(1);
+    arr.display(); 
+} 
+}
---
+++ dataArrayApp.java
@@ -0,0 +1,90 @@
+class Mahasiswa{
+    private long nim;
+    private String nama;
+    private String asal;
+    public Mahasiswa(long nim, String nama, String asal){
+        this.nama = nama;
+        this.nim = nim;
+        this.asal = asal;
+    }
+    public void displayMhs(){
+        System.out.println("\tnim: " + nim);
+        System.out.println("\tnama: " + nama);
+        System.out.println("\tasal: " + asal);
+    }
+    public long getNim(){
+        return nim;
+    }
+}
+
+class DataArray{
+    private Mahasiswa[] mhs;
+    private int nElemen;
+    public DataArray(int max){
+        mhs = new Mahasiswa[max];
+        nElemen = 0;
+    }
+    public Mahasiswa find(long searchNim){
+        for (int i = 0; i < nElemen; i++) {
+            if(mhs[i].getNim() == searchNim){
+                return mhs[i];
+            }
+        }
+        return null;
+    }
+    public void insert(long nim, String nama, String asal){
+        mhs[nElemen]  = new Mahasiswa(nim, nama, asal);
+        nElemen++;
+    }
+    public boolean delete(long searchNim){
+        for (int i = 0; i < nElemen; i++) {
+            if(mhs[i].getNim()==searchNim){
+                break;
+            }
+            if(i == nElemen){
+                return false;
+            }else{
+                for (int j = 0; j < nElemen; j++) {
+                    mhs[j] = mhs[j+1];
+                }
+                nElemen--;
+                return true;
+            }
+        }return false;
+    }
+    public void display(){
+        for (int i = 0; i < nElemen; i++) {
+            mhs[i].displayMhs();
+        }
+    }
+}
+
+class DataArrayApp {
+    public static void main(String[] args) {
+        int maxSize = 100;
+        DataArray arr;
+        arr = new DataArray(maxSize);
+        arr.insert(16650200, "jundi", "malang");
+        arr.insert(16650210, "Ahmad", "sidoarjo");
+        arr.insert(16650220, "Ismail", "Banyuwangi");
+        arr.insert(16650230, "sofi", "semarang");
+        arr.insert(16650240, "dinda", "bandung");
+        arr.insert(16650250, "rais", "ambon");
+        arr.insert(16650260, "helmi", "madura");
+        arr.insert(16650270, "agung", "madiun");
+        arr.insert(16650280, "arina", "malang");
+        arr.display();
+        long searchKey =  16650270;
+        Mahasiswa mhs = arr.find(searchKey);
+        if(mhs != null){
+            System.out.println("ketemu");
+            mhs.displayMhs();
+        }else{
+            System.out.println("tidak ketemu" + searchKey);
+        }
+        searchKey = 16650240;
+        System.out.println("\n hapus nim :" + searchKey +"\n");
+        arr.delete(searchKey);
+        arr.display();
+    }
+}
